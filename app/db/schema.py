c2313b8c7623c5d4dded1dd96de95ecbc0093ef6
strandafredde <strandafredde@gmail.com>
2024-01-17 19:37:10 +0100

    Added missing stuff
    Added missing Foreign Keys
    Added Missing Category table
    Added missing relationships

--- app/db/schema.py
+++ app/db/schema.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -16,6 +18,14 @@ class User(db.Model):
         self.email = email
         self.password = password
 
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(32))
+
+    def __init__(self, category_name):
+        self.category_name = category_name
+
 class Products(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
@@ -23,7 +33,7 @@ class Products(db.Model):
     price = db.Column(db.Float)
     description = db.Column(db.String(64))
     image = db.Column(db.String(64))
-    category = db.Column(db.String(32))
+    category = db.Column(db.Integer, ForeignKey('category.id'))
 
     def __init__(self, name, price, description, image, category):
         self.name = name
@@ -35,8 +45,11 @@ class Products(db.Model):
 class Cart(db.Model):
     __tablename__ = 'cart'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    product_id = db.Column(db.Integer, ForeignKey('products.id'))
+
+    user = relationship('User', backref='carts')
+    product = relationship('Products', backref='carts')
 
     def __init__(self, user_id, product_id):
         self.user_id = user_id
@@ -45,13 +58,17 @@ class Cart(db.Model):
 class Orders(db.Model):
     __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer)
-    product_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    product_id = db.Column(db.Integer, ForeignKey('products.id'))
     quantity = db.Column(db.Integer)
     total_price = db.Column(db.Float)
+
+    user = relationship('User', backref='orders')
+    product = relationship('Products', backref='orders')
 
     def __init__(self, user_id, product_id, quantity, total_price):
         self.user_id = user_id
         self.product_id = product_id
         self.quantity = quantity
         self.total_price = total_price
+
